476932132cb82fd53b4c1a1d102747ba980125e5
joedysonbezerra <joedysonbezerra@gmail.com>
2019-09-19 07:47:03 -0300

    alterações necessárias

---
+++ src/Repository.js
@@ -0,0 +1,69 @@
+import api from "./api";
+
+class Repository {
+  async search(repositoryName) {
+    const {
+      data: {
+        name,
+        description,
+        owner: { avatar_url },
+        html_url,
+        stargazers_count,
+        forks_count,
+        open_issues,
+        pushed_at
+      }
+    } = await api.get(`${repositoryName}`);
+    return {
+      name,
+      description,
+      avatar_url,
+      html_url,
+      stargazers_count,
+      forks_count,
+      open_issues,
+      pushed_at
+    };
+  }
+
+  render(
+    {
+      avatar_url,
+      name,
+      description,
+      stargazers_count,
+      forks_count,
+      open_issues,
+      pushed_at,
+      html_url
+    },
+    index
+  ) {
+    const repository = document.createElement("li");
+    repository.innerHTML = `
+    <img src="${avatar_url}" alt="Icone do Repositório">
+                      <strong>${name}</strong>
+                     <p>${description || "sem descrição"}</p>
+                      <div id="details">
+                        <p> <i class="material-icons">
+                        stars
+                        </i> ${stargazers_count || "0"}</p>
+                        <p> <i class="material-icons">
+                        share
+                        </i> ${forks_count || "0"}</p>
+                        <p> <i class="material-icons">
+                        report_problem
+                        </i> ${open_issues || "0"}</p>
+                        <p> <i class="material-icons">
+                        access_time
+                        </i> Last Commit: ${pushed_at}</p>
+                        <a href="${html_url}" target="_blank">Acessar</a>
+                      </div>
+                      <hr/>
+        `;
+
+    return repository;
+  }
+}
+
+export default new Repository();
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import api from "./api";
+import Repository from "./Repository";
 
 class App {
   constructor() {
@@ -6,103 +6,40 @@ class App {
 
     this.formEl = document.getElementById("repo-form");
     this.inputEl = document.querySelector("input[name=repository]");
-    this.listEl = document.getElementById("repo-list");
+    this.listRepositories = document.getElementById("repo-list");
+
+    this.repository = Repository;
 
     this.registerHandlers();
   }
 
   registerHandlers() {
-    this.formEl.onsubmit = event => this.addRepository(event);
+    this.formEl.onsubmit = event => this.handleAddRepository(event);
   }
 
-  async addRepository(event) {
+  async handleAddRepository(event) {
     event.preventDefault();
-
     const repoInput = this.inputEl.value;
 
-    if (repoInput.length === 0) return;
-
-    const response = await api.get(`/${repoInput}`);
-
-    const {
-      name,
-      description,
-      html_url,
-      owner: { avatar_url },
-      stargazers_count,
-      forks_count,
-      open_issues,
-      pushed_at
-    } = response.data;
-
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url,
-      stargazers_count,
-      forks_count,
-      open_issues,
-      pushed_at
-    });
+    if (repoInput.length === 0) return alert("Digite algum repositório valído");
 
     this.inputEl.value = "";
 
+    this.repositories.push(await this.repository.search(repoInput));
     this.render();
   }
 
   render() {
-    this.listEl.innerHTML = "";
-
-    this.repositories.forEach(repo => {
-      let imgEl = document.createElement("img");
-      imgEl.setAttribute("src", repo.avatar_url);
-
-      let titleEl = document.createElement("strong");
-      titleEl.appendChild(document.createTextNode(repo.name));
-
-      let descriptionEl = document.createElement("p");
-      descriptionEl.appendChild(
-        document.createTextNode(`${repo.description || "sem descrição"}`)
-      );
-
-      let starsEl = document.createElement("p");
-      starsEl.appendChild(
-        document.createTextNode(`Stars: ${repo.stargazers_count || "0"}`)
-      );
-
-      let forksEl = document.createElement("p");
-      forksEl.appendChild(
-        document.createTextNode(`Forks: ${repo.forks_count || "0"}`)
-      );
-
-      let issuesEl = document.createElement("p");
-      issuesEl.appendChild(
-        document.createTextNode(`Issues: ${repo.open_issues || "0"}`)
-      );
-
-      let lastCommitEl = document.createElement("p");
-      lastCommitEl.appendChild(
-        document.createTextNode(`Last Commit: ${repo.pushed_at}`)
-      );
-
-      let linkEl = document.createElement("a");
-      linkEl.setAttribute("target", "_blank");
-      linkEl.setAttribute("href", repo.html_url);
-      linkEl.appendChild(document.createTextNode("Acessar"));
-
-      let listItemEl = document.createElement("li");
-      listItemEl.appendChild(imgEl);
-      listItemEl.appendChild(titleEl);
-      listItemEl.appendChild(descriptionEl);
-      listItemEl.appendChild(starsEl);
-      listItemEl.appendChild(forksEl);
-      listItemEl.appendChild(issuesEl);
-      listItemEl.appendChild(lastCommitEl);
-      listItemEl.appendChild(linkEl);
-
-      this.listEl.appendChild(listItemEl);
+    this.listRepositories.innerHTML = "";
+    this.exclude = this.exclude.bind(this);
+    this.repositories.forEach(repository => {
+      this.listRepositories.appendChild(close);
+      this.listRepositories.appendChild(this.repository.render(repository));
     });
+  }
+  exclude(index) {
+    console.log("ok");
+    //this.render();
   }
 }
 
